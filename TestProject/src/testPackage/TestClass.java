e88ac1cf196d9cfea3b88ce0cffa096e8eef7d98
PrajayShinde <136174710+PrajayShinde@users.noreply.github.com>
2023-06-11 16:27:19 +0530

    Update TestClass.java

--- TestProject/src/testPackage/TestClass.java
+++ TestProject/src/testPackage/TestClass.java
@@ -4,6 +4,11 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a=10;
+		int b=5;
+		int c= a+b;
+		System.out.print(c);
+		
 
 	}
 
